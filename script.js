26cfc35a2d5187edf0aefb967ddeec3f9f8d04c5
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-11-20 06:13:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,60 +2,43 @@
 
 const datas = [
     {
-        'title': 'One row', 'sum': 100
+        'title': 'One row', 'sum': 100, 'bonus': 30
     },
     {
-        'title': 'Two row', 'sum': 200
+        'title': 'Two row', 'sum': 200, 'bonus': 10
     },
     {
-        'title': 'Three row', 'sum': 300
+        'title': 'Three row', 'sum': 300, 'bonus': 20
     },
     {
-        'title': 'Foure row', 'sum': 400
+        'title': 'Foure row', 'sum': 400, 'bonus': 5
     },
     {
-        'title': 'Five row', 'sum': 500
+        'title': 'Five row', 'sum': 500, 'bonus': 3
     },
     {
-        'title': 'Six row', 'sum': 600
+        'title': 'Six row', 'sum': 600, 'bonus': 50
     },
-]
+    {
+        'title': 'Seven row', 'sum': 700, 'bonus': 40
+    },
+];
+
 const table = document.querySelector('#table');
-let sum = 0;
+let sum = 0, sumBonus = 0;
 
 for(const data of datas) {
     let tr = document.createElement('tr');
-    tr.innerHTML = `<td>${data.title}</td><td>${data.sum}</td>`;
+    tr.innerHTML = `<td>${data.title}</td>
+                    <td>${data.sum}</td>
+                    <td>${data.bonus}</td>`;
     table.append(tr);
     sum = sum + (+data.sum);
+    sumBonus = sumBonus + (+data.bonus);
 }
 
 let trSum = document.createElement('tr');
-trSum.innerHTML += `<td class="total">Total: </td><td class="sum-result">${sum}</td>`;
+trSum.innerHTML += `<td class="total">Total sum ${datas.length} elements:</td>
+                    <td class="sum-result">${sum}</td>
+                    <td class="sum-result">${sumBonus}</td>`;
 table.append(trSum);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
