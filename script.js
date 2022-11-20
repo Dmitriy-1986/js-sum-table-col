d0e1bb2f5ea9dbdcda5fd415c51f93faaf282f9a
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-11-20 06:24:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -27,18 +27,19 @@ const datas = [
 const table = document.querySelector('#table');
 let sum = 0, sumBonus = 0;
 
-for(const data of datas) {
+for(let data = 0; data < datas.length; data++) {
     let tr = document.createElement('tr');
-    tr.innerHTML = `<td>${data.title}</td>
-                    <td>${data.sum}</td>
-                    <td>${data.bonus}</td>`;
+    tr.innerHTML = `<td>${data + 1}</td>
+                    <td>${datas[data].title}</td>
+                    <td>${datas[data].sum}</td>
+                    <td>${datas[data].bonus}</td>`;
     table.append(tr);
-    sum = sum + (+data.sum);
-    sumBonus = sumBonus + (+data.bonus);
+    sum = sum + (+datas[data].sum);
+    sumBonus = sumBonus + (+datas[data].bonus);
 }
 
 let trSum = document.createElement('tr');
-trSum.innerHTML += `<td class="total">Total sum ${datas.length} elements:</td>
+trSum.innerHTML += `<td class="total" colspan="2">Total sum ${datas.length} elements:</td>
                     <td class="sum-result">${sum}</td>
                     <td class="sum-result">${sumBonus}</td>`;
 table.append(trSum);
